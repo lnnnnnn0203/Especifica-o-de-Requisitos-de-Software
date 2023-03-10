0a523389f8cb0bb5e9261a64f8d6dd39e7eed604
lnnnnnn0203 <122923313+lnnnnnn0203@users.noreply.github.com>
2023-03-10 02:14:40 -0300

    Create Template.cs

---
+++ Template.cs
@@ -0,0 +1,18 @@
+namespace Template; 
+
+public class Template
+{
+  public int data { get; set; }
+public string Versão { get; set; }
+public string descrição { get; set; }
+public string autor { get; set; }
+
+public Template(int data, string Versão, string descrição, string autor)
+{
+  this.data = data;
+  this.Versão = Versão;
+  this.descrição = descrição;
+  this.autor = autor;
+    
+}
+}
